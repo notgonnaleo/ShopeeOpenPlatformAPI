d5914fb1d82cbd8c7302e77b2044f917acc5c7c0
Leonardo Bruni <leonardo.bruni@topssoft.com>
2023-03-20 12:51:11 -0300

    cool changes im tired of doing this

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const axios = require("axios");
 const https = require('https');
+const crypto = require('crypto');
 const app = express()
 const PORT = 3000;
 
@@ -22,8 +23,8 @@ app.listen(PORT, function(err){
 // Authenticate Shopee API and User information.
 app.get('/', async (req, res) => {
     try {
-        const host = "https://partner.shopeemobile.com";
-        const apiPath = "/api/v2/shop/auth_partner?"
+        const host = "https://partner.test-stable.shopeemobile.com";
+        const apiPath = "/api/v2/shop/auth_partner"
 
         // TODO: Encontrar valor do secret_key, verificar se o partnerKey é a mesma coisa que partner_id, caso não seja corrigir.
         // Favor adicionar todos os parametros necessarios de acordo com a documentação
@@ -32,24 +33,28 @@ app.get('/', async (req, res) => {
 
         let unixTimestamp = Math.floor(Date.now()/1000);
         const partnerKey = "d1f09dbf3f8c2e71bd160f0baa9cdce8013443fdfd483644bf5b3bc4aaef17f4";
-        const partnerId = 1008742;
+        const partnerId = parseInt(1008742);
         const shopId = 78299;
 
         // const secret_key = "";
         const redirectUrl = "https://jetcontrol-prod.azurewebsites.net/dashboard/dashboard-analytics"
 
-        let rawSign = partnerKey + apiPath + unixTimestamp
-        let signSHA256 = await crypto.subtle.digest("SHA-256", rawSign)  //mesma coisa para o secret key mas vou passar o partherID nolugar do rawSign
-        const signArray = Array.from(new Uint8Array(signSHA256));
-        const sign = signArray
-          .map((item) => item.toString(16).padStart(2, "0"))
-          .join("");
+        let baseString = partnerId + apiPath + unixTimestamp
+        console.log("baseString ==" + baseString);
+        
+        let hash = await crypto.subtle.digest("SHA-256", baseString, partnerKey)  //mesma coisa para o secret key mas vou passar o partherID nolugar do rawSign
+        const sign = Array
+            .from(new Uint8Array(hash))
+            .map((item) => item.toString(16).padStart(2, "0"))
+            .join("");
+
+        console.log("sign==" + sign);
 
         let authenticationEndpoint = host + apiPath 
-        + 'partner_id=' + partnerId 
-        + 'timestamp=' + unixTimestamp 
-        + 'sign=' + sign 
-        + 'redirect=' + redirectUrl
+        + '?partner_id=' + partnerId
+        + '&timestamp=' + unixTimestamp 
+        + '&sign=' + sign 
+        + '&redirect=' + redirectUrl
 
         console.log(authenticationEndpoint);
 
